c5602fe93b5c503a3f908ea8c6e6b5b8f2f286b8
Anup Mahindre <anup.mahindre.7@gmail.com>
2024-08-03 14:23:28 +0530

    Assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,13 +12,14 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+TOOLCHAIN_SYS_ROOT=$(aarch64-none-linux-gnu-gcc -print-sysroot -v 2>/dev/null)
 
 if [ $# -lt 1 ]
 then
-	echo "Using default directory ${OUTDIR} for output"
+    echo "Using default directory ${OUTDIR} for output"
 else
-	OUTDIR=$1
-	echo "Using passed directory ${OUTDIR} for output"
+    OUTDIR=$1
+    echo "Using passed directory ${OUTDIR} for output"
 fi
 
 mkdir -p ${OUTDIR}
@@ -26,55 +27,83 @@ mkdir -p ${OUTDIR}
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+    git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/vmlinux ${OUTDIR}/
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
+# cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/zImage ${OUTDIR}/
+# cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/uImage ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+mkdir -p $OUTDIR/rootfs
+cd $OUTDIR/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp
+mkdir -p usr/bin usr/lin usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs/ ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
+cd ${OUTDIR}/rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+cp ${TOOLCHAIN_SYS_ROOT}/lib/ld-linux-aarch64.so.1 ./lib/
 
-# TODO: Make device nodes
+cp ${TOOLCHAIN_SYS_ROOT}/lib64/libm.so.6 ./lib64/
+cp ${TOOLCHAIN_SYS_ROOT}/lib64/libresolv.so.2 ./lib64/
+cp ${TOOLCHAIN_SYS_ROOT}/lib64/libc.so.6 ./lib64/
 
-# TODO: Clean and build the writer utility
+sudo mknod -m 666 ./dev/null c 1 3
+sudo mknod -m 600 ./dev/console c 5 1
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+make -C ${FINDER_APP_DIR} CROSS_COMPILE=${CROSS_COMPILE} clean
+make -C ${FINDER_APP_DIR} CROSS_COMPILE=${CROSS_COMPILE}
 
-# TODO: Chown the root directory
+cp ${FINDER_APP_DIR}/finder.sh ./home/
+cp ${FINDER_APP_DIR}/finder-test.sh ./home/
+cp ${FINDER_APP_DIR}/writer ./home/
+cp ${FINDER_APP_DIR}/autorun-qemu.sh ./home/
+mkdir -p ./home/conf
+cp ${FINDER_APP_DIR}/../conf/* ./home/conf/
+sed -i 's?../conf/assignment.txt?conf/assignment.txt?g' ./home/finder-test.sh
 
-# TODO: Create initramfs.cpio.gz
+sudo chown -R root:root *
+
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
